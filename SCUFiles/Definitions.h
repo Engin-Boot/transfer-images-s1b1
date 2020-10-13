fa17fa3b359468b560be4312208cb60621f187b1
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-10-13 16:56:35 +0530

    Update added more paths

--- SCUFiles/Definitions.h
+++ SCUFiles/Definitions.h
@@ -24,7 +24,11 @@ using namespace std;
 #include "../mc3inc/diction.h"
 #include "../mc3inc/mcstatus.h"
 #include "../mc3inc/mc3msg.h"
-
+#include "../mc3inc/mc3items.h"
+#include "../mc3inc/mc3services.h"
+#include "../mc3inc/pcd.h"
+#include "../mc3inc/pic.h"
+#include "../mc3inc/stdtypes.h"
 
 #include "../mc3inc/general_util.h"
 

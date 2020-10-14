136a8eb9669eb066577b26c8f9633304d0322967
Tanmay Bhatt <tanmaybhatt211@gmail.com>
2020-10-14 21:54:11 +0530

    function addition in defs

--- SCUFiles/Definitions.h
+++ SCUFiles/Definitions.h
@@ -178,11 +178,16 @@ int GetNumNodes(InstanceNode* A_list);
 //Image Read and Send related functions
 
 FORMAT_ENUM CheckFileFormat(char* A_filename);
+FORMAT_ENUM CheckSignatureOfMediaFile(FILE*& fp);
+
 SAMP_BOOLEAN ReadImage(STORAGE_OPTIONS* A_options, int A_appID, InstanceNode* A_node);
 void ValidImageCheck(InstanceNode* A_node);
 MC_STATUS CreateEmptyFileAndStoreIt(int& A_appID, int*& A_msgID, char*& A_filename, CBinfo& callbackInfo);
 SAMP_BOOLEAN SendImage(STORAGE_OPTIONS* A_options, int A_associationID, InstanceNode* A_node);
+
 MC_STATUS NOEXP_FUNC MediaToFileObj(char* Afilename, void* AuserInfo, int* AdataSize, void** AdataBuffer, int AisFirst, int* AisLast);
+bool CheckTransferSyntax(int A_syntax);
+void CloseCallBackInfo(CBinfo& callbackInfo);
 bool Transfer_Syntax_Encoding(MC_STATUS mcStatus, int*& A_msgID, TRANSFER_SYNTAX*& A_syntax);
 bool Image_Extraction(int*& A_msgID, TRANSFER_SYNTAX*& A_syntax, char*& A_filename, char* sopClassUID, char* sopInstanceUID, size_t& size_sopClassUID, size_t& size_sopInstanceUID);
 bool Message_Creation(MC_STATUS mcStatus, int*& A_msgID, char* sopClassUID, char* sopInstanceUID, size_t& size_sopClassUID, size_t& size_sopInstanceUID);
@@ -190,6 +195,16 @@ bool Syntax_Handling(MC_STATUS mcStatus, int*& A_msgID, TRANSFER_SYNTAX*& A_synt
 bool Message_Handling(int*& A_msgID, char* sopClassUID, char* sopInstanceUID);
 bool ReadFile1(int& A_appID, char*& A_filename, int*& A_msgID, TRANSFER_SYNTAX*& A_syntax, size_t*& A_bytesRead);
 bool ReadFile2(int*& A_msgID, TRANSFER_SYNTAX*& A_syntax, char*& A_filename);
+
+int GetWorkBufferSize();
+bool AllocateBuffer(CBinfo*& callbackInfo);
+void checkIfBufferSet(int retStatus);
+bool firstCallProcedure(char*& A_filename, CBinfo*& callbackInfo, int& retStatus, int& A_isFirst);
+bool SetBuffer(char*& A_filename, CBinfo*& callbackInfo, int& retStatus, int& A_isFirst, void* A_userInfo);
+bool closeCallBackFile(CBinfo*& callbackInfo, int*& A_isLast);
+bool ReadInCallBackFile(CBinfo*& callbackInfo, size_t& bytes_read, int*& A_isLast);
+
+
 
 void PrintError(const char* A_string, MC_STATUS A_status);
 bool CheckIfMCStatusNotOk(MC_STATUS mcStatus, const char* ErrorMessage);
@@ -203,6 +218,7 @@ SAMP_BOOLEAN ReadFileFromMedia(STORAGE_OPTIONS* A_options,
     int* A_msgID,
     TRANSFER_SYNTAX* A_syntax,
     size_t* A_bytesRead);
+
 
 //Main working class
 
@@ -263,5 +279,4 @@ public:
     void RemoteVerbose();
     void VerboseAfterConnection();
     void VerboseTransferSyntax();
-
 };
